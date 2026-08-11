63033513ec7c8c5002e5732f061e68a73ce43903
ranjansingh24 <singhranjan.net@gmail.com>
2026-08-11 12:13:14 +0530

    Update provider version and Azure backend configuration

--- Parent-Moudle/provider.tf
+++ Parent-Moudle/provider.tf
@@ -2,22 +2,22 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.80.0"
+      version = "~> 5.0"
     }
   }
 }
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "rg-backend"
-#     storage_account_name = "singhranjan1724"
-#     container_name       = "backend-container"
-#     key                  = "terraform.tfstate"
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "Ranjan_24"
+    storage_account_name = "ranjansingh24"
+    container_name       = "ranjancontainer"
+    key                  = "ranjan.tfstate"
 
-#   }
-# }
+  }
+}
 provider "azurerm" {
   features {}
-  subscription_id = "a2d0788b-89e9-49c6-8c6a-5f152ef8d304"
+  subscription_id = "cf4adfd0-252d-4813-b002-f6f2095a23a8"
 }
 
 
